b5715682091204fb942fdf261a4791a4dbb7229d
satyam <satyamtech42@gmail.com>
2023-07-09 18:26:25 +0300

    tf models can now be sent

--- client/script.py
+++ client/script.py
@@ -5,75 +5,86 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from scipy .special import softmax
+import tensorflow as tf
 
-filename = "/usr/data/model.txt"
+#ilename = "/usr/data/model.txt"
 
-class MCLogisticRegressor:
-    def __init__(self) -> None:
-        pass
+def create_model():
+  model = tf.keras.Sequential([
+    tf.keras.layers.Dense(512, activation='relu', input_shape=(784,)),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)
+  ])
 
-    def train(self, X, Y) -> None:
-        self.encoding_map = dict.fromkeys(np.unique(Y))
-        for k, ele in enumerate(self.encoding_map.keys()):
-            self.encoding_map[ele] = k
+  model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-        self.decoded_map = {}
-        for k, ele in enumerate(self.encoding_map.keys()):
-            self.decoded_map[k] = ele
-        
-        self.W = self.__gradient_descent__(X, Y)
+  return model
 
-    def __gradient_descent__(self, X, Y, max_iterations=1000, alpha=0.1, mu=0.01):
-        y_onehot_encoded = self.__onehot_encoded__(Y)
-        W = np.zeros((X.shape[1], y_onehot_encoded.shape[1]))
+##class MCLogisticRegressor:
+##    def __init__(self) -> None:
+##        pass
+##
+##    def train(self, X, Y) -> None:
+##        self.encoding_map = dict.fromkeys(np.unique(Y))
+##        for k, ele in enumerate(self.encoding_map.keys()):
+##            self.encoding_map[ele] = k
+##
+##        self.decoded_map = {}
+##        for k, ele in enumerate(self.encoding_map.keys()):
+##            self.decoded_map[k] = ele
+##        
+##        self.W = self.__gradient_descent__(X, Y)
+##
+##    def __gradient_descent__(self, X, Y, max_iterations=1000, alpha=0.1, mu=0.01):
+##        y_onehot_encoded = self.__onehot_encoded__(Y)
+##        W = np.zeros((X.shape[1], y_onehot_encoded.shape[1]))
+##
+##        for i in range(max_iterations):
+##            W -= alpha * self.__gradient__(X, y_onehot_encoded, W, mu)
+##        
+##        return W
 
-        for i in range(max_iterations):
-            W -= alpha * self.__gradient__(X, y_onehot_encoded, W, mu)
-        
-        return W
-
-    def __gradient__(self, X, Y, W, mu):
-        Z = X @ W
-        P = softmax(Z, axis=1)
-        grad = (-1 / X.shape[0]) * (X.T @ (Y - P)) + 2 * mu * W
-        return grad        
-
-    def __loss__(self, X, Y, W):
-        Z = X @ W
-        loss = (1 / X.shape[0]) * (-np.trace(X @ W @ Y.T) + np.sum(np.log(np.sum(np.exp(Z), axis=1))))
-        return loss
-
-    def __onehot_encoded__(self, y):
-        y_onehot_encoded = np.zeros((y.shape[0], len(self.encoding_map.keys())))
-        for i, ele in enumerate(y_onehot_encoded):
-            ele[self.encoding_map[y[i]]] = 1
-        
-        return y_onehot_encoded
-
+##   def __gradient__(self, X, Y, W, mu):
+##       Z = X @ W
+##       P = softmax(Z, axis=1)
+##       grad = (-1 / X.shape[0]) * (X.T @ (Y - P)) + 2 * mu * W
+##       return grad       
+##   def __loss__(self, X, Y, W):
+##       Z = X @ W
+##       loss = (1 / X.shape[0]) * (-np.trace(X @ W @ Y.T) + np.sum(np.log(np.sum(np.exp(Z), axis=1))))
+##       return loss
+##   def __onehot_encoded__(self, y):
+##       y_onehot_encoded = np.zeros((y.shape[0], len(self.encoding_map.keys())))
+##       for i, ele in enumerate(y_onehot_encoded):
+##           ele[self.encoding_map[y[i]]] = 1
+##       
+##       return y_onehot_encoded
 def trainAndSaveModel():
-    csv_data = pd.read_csv("/usr/data/iris.data")
-    X = csv_data.iloc[:, :-1].values
-    Y = csv_data.iloc[:, -1].values
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    assert x_train.shape == (60000, 28, 28)
+    assert x_test.shape == (10000, 28, 28)
+    assert y_train.shape == (60000,)
+    assert y_test.shape == (10000,)
+    x_train = x_train.reshape(-1,28*28) / 255.0
+    x_test = x_test.reshape(-1,28*28) / 255.0
+    model = create_model()
+    model.fit(x_train,y_train,epochs = 1,validation_data = (x_test,y_test))
+    return model
 
-    X_train, _, Y_train, __ = train_test_split(X, Y, test_size=0.4, random_state=42)
-    logreg = MCLogisticRegressor()
-    logreg.train(X_train, Y_train)
 
-    with open(filename, 'w') as f:
-        np.savetxt(f, logreg.W, fmt='%.3f')
+    
 
 def client():
     url = 'http://server:8000/server'  # Replace with the server URL
-    message = ""
-    
-    if os.path.exists(filename):
-        file = open(filename, "r")
-        message = file.read()
-        file.close()
-    else:
-        print("Failed")
-    
-    response = requests.post(url, {"message": message})
+    model = trainAndSaveModel()
+    w = model.get_weights()
+    for i in range(0,len(w)):
+          w[i] =  w[i].tolist()
+    response = requests.post(url, json = {"weights" : w,"config" : model.get_config()})
+
+
     if response.status_code == 200:
         print('Server response:', response.text)
     else:
--- server/script.py
+++ server/script.py
@@ -3,55 +3,61 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from scipy .special import softmax
+import tensorflow as tf
 
-class MCLogisticRegressor:
-    def __init__(self, X, Y, W) -> None:
-        self.W = W
-
-        self.encoding_map = dict.fromkeys(np.unique(Y))
-        for k, ele in enumerate(self.encoding_map.keys()):
-            self.encoding_map[ele] = k
-            
-        self.decoded_map = {}
-        for k, ele in enumerate(self.encoding_map.keys()):
-            self.decoded_map[k] = ele
-    
-    def predict(self, X):
-        Z = X @ self.W
-        P = softmax(Z, axis=1)
-        encoded_predictions = np.argmax(P, axis=1)
-        return np.array([self.decoded_map[x] for x in encoded_predictions])
+##class MCLogisticRegressor:
+##    def __init__(self, X, Y, W) -> None:
+##        self.W = W
+##
+##        self.encoding_map = dict.fromkeys(np.unique(Y))
+##        for k, ele in enumerate(self.encoding_map.keys()):
+##            self.encoding_map[ele] = k
+##            
+##        self.decoded_map = {}
+##        for k, ele in enumerate(self.encoding_map.keys()):
+##            self.decoded_map[k] = ele
+##    
+##    def predict(self, X):
+##        Z = X @ self.W
+##        P = softmax(Z, axis=1)
+##        encoded_predictions = np.argmax(P, axis=1)
+##        return np.array([self.decoded_map[x] for x in encoded_predictions])
 
 app = Flask(__name__)
 
-def predict(w):
-    csv_data = pd.read_csv("/usr/data/iris.data")
-    X = csv_data.iloc[:, :-1].values
-    Y = csv_data.iloc[:, -1].values
-
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
-
-    logreg = MCLogisticRegressor(X_train, Y_train, w)
-    predictions = logreg.predict(X_test)
-    return np.sum(predictions == Y_test) / Y_test.shape[0] * 100
+##def predict(w):
+##    csv_data = pd.read_csv("/usr/data/iris.data")
+##    X = csv_data.iloc[:, :-1].values
+##    Y = csv_data.iloc[:, -1].values
+##
+##    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
+##
+##    logreg = MCLogisticRegressor(X_train, Y_train, w)
+##    predictions = logreg.predict(X_test)
+##    return np.sum(predictions == Y_test) / Y_test.shape[0] * 100
 
 @app.route('/server', methods=['POST'])
 def server():
     if request.method == 'POST':
-        message = request.form.get('message', '')
-
-        with open("/usr/data/temp.txt", 'w') as f:
-            f.write(message)
+        config = request.json["config"]
+        model = tf.keras.Sequential().from_config(config)
+        weights = request.json["weights"]
+        for i in range(0,len(weights)):
+            weights[i] =  np.array(weights[i])
         
-        w = None
-        with open("/usr/data/temp.txt", "r") as f:
-            w = np.loadtxt(f)
-
-        accuracy = predict(w)
-        message =  "Accuracy = " + str(accuracy) + "%"
+        model.set_weights(weights)
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+        assert x_train.shape == (60000, 28, 28)
+        assert x_test.shape == (10000, 28, 28)
+        assert y_train.shape == (60000,)
+        assert y_test.shape == (10000,)
+        x_train = x_train.reshape(-1,28*28) / 255.0
+        x_test = x_test.reshape(-1,28*28) / 255.0
+        model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+        return "evaluated " + str(model.evaluate(x_test,y_test))
         
-        print('Received message from client:', message)
-        return 'Message received by the server:' + message
     else:
         return 'Invalid request method'
 
