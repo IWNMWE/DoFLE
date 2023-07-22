66557df4896e9c752f5e1205c266e7c8eab0ee60
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-07-23 00:18:12 +0530

    fixed client port configs

--- client/script.py
+++ client/script.py
@@ -104,5 +104,5 @@ if __name__ == '__main__':
     cli = client(modelfile,datafolder)
     cli.train()
     cli.send_server()
-    app.run(host = '0.0.0.0',port = 5000)
+    app.run(host = '0.0.0.0',port = 8000)
 
