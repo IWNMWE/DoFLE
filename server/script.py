e8cf79007161abe1d2f1130733a6aee5504373ae
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-07-23 00:16:28 +0530

    implemented async function to send model to clients

--- server/script.py
+++ server/script.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from scipy .special import softmax
 import tensorflow as tf
+import requests
+import asyncio
 
 app = Flask(__name__)
 
@@ -24,6 +26,45 @@ def FSGD(weights,config):
     return new_model 
 
 
+async def sendCurrentModel(clients, model, retry=True, timeout=300, numTimes=1):
+    """Sends current model to clients asynchronously.
+
+    Sends a POST request to [clients] to acknowledge they are online
+    along with the current model [model] for a current iteration.
+    In case client(s) do not respond (offline), they are flagged to be
+    requested again after every [timeout] seconds for [numTimes] if
+    [retry] == True.
+
+    Args:
+        clients: list of chosen clients
+        W: current model (weights)
+        retry: whether to retry failed requests
+        timeout: seconds to wait before retrying
+        numTimes: number of times to retry
+    """
+    for i in range(1 + numTimes):
+        retryClients = []            
+        for client in clients:
+            url = f'http://{client}:8000/client'
+            w = model.get_weights()
+            for i in range(0,len(w)):
+                w[i] =  w[i].tolist()
+            response = requests.post(url, json = {"weights" : w,"config" : model.get_config()})
+
+            if response.status_code == 200:
+                print(f'Client - {client} received model')
+            else:
+                if(retry == True):
+                    retryClients.append(client)
+                print('Error:', response.status_code)
+        
+        # Break if [retry] = False or ifall requests were successful
+        if(retry == False or len(retryClients) == 0):
+            break
+        # Sleep for [timeout] seconds and retry
+        else:
+            await asyncio.sleep(timeout)
+            clients = retryClients
 
 
 @app.route('/server', methods=['POST'])
